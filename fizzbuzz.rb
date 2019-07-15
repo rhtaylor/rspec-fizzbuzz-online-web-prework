4a65c4b60179f71c991ba2ac026b78f9db015c20
Randall Taylor <randyisbusy@gmail.com>
2019-07-15 05:25:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(arg)
+  if arg%3 && arg % 5
+    puts "fizzbuzz"
+  elsif arg%3
+    puts "fizz"
+  elsif arg%5
+    puts "buzz"
+  else nil
+  end
+end 
+ fizzbuzz(15)
