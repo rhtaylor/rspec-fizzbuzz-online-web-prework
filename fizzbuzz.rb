9778d4546c1fae9401ad206958b6a8468ae5b14c
Randall Taylor <randyisbusy@gmail.com>
2019-07-15 05:49:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(arg)
   if (arg%3 == 0) && (arg % 5 == 0)
-    return "fizzbuzz"
+    return "FizzBuzz"
   elsif arg%3 == 0
     return "Fizz"
   elsif arg%5 == 0
